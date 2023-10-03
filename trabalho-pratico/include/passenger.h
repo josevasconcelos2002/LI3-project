02d95ab95d3a3d0add6f85795670cc51eb1a0220
Jose Vasconcelos <zevasconcelos6@gmail.com>
2023-10-03 17:34:20 +0100

    Passenger

---
+++ trabalho-pratico/include/passenger.h
@@ -0,0 +1,9 @@
+#ifndef PASSENGER_H
+#define PASSENGER_H
+
+typedef struct passenger Passenger;
+
+int get_passenger_flight_id(Passenger *p);
+int get_passenger_user_id(Passenger *p);
+
+#endif
---
+++ trabalho-pratico/src/passenger.c
@@ -0,0 +1,18 @@
+#include "passenger.h"
+#include <string.h>
+
+struct passenger
+{
+    int flight_id;
+    int user_id;
+};
+
+int get_passenger_flight_id(Passenger *p)
+{
+    return p->flight_id;
+}
+
+int get_passenger_user_id(Passenger *p)
+{
+    return p->user_id;
+}
