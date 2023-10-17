a6aef7a9ce521b7a3e2ef9c401a78fa192693b81
Jose Vasconcelos <zevasconcelos6@gmail.com>
2023-10-17 17:24:29 +0100

    Mais funcoes para as hashtables

--- trabalho-pratico/include/hashtables.h
+++ trabalho-pratico/include/hashtables.h
@@ -11,6 +11,22 @@ typedef struct catalogo_reservations catalogo_Reservations;
 
 typedef struct catalogo_users catalogo_Users;
 
+catalogo_Flights *init_catalogo_flights();
+
+catalogo_Passengers *init_catalogo_passengers();
+
+catalogo_Reservations *init_catalogo_reservations();
+
+catalogo_Users *init_catalogo_users();
+
+void free_catalogo_flights(catalogo_Flights *cat_flights);
+
+void free_catalogo_passengers(catalogo_Passengers *cat_passengers);
+
+void free_catalogo_reservations(catalogo_Reservations *cat_reservations);
+
+void free_catalogo_users(catalogo_Users *cat_users);
+
 const GHashTable *get_table_flights(const catalogo_Flights *cat_flights);
 
 const GHashTable *get_table_passengers(const catalogo_Passengers *cat_passengers);
